0d89fcc4559455c81a5394ab9dca52ce0ba6013e
Ram <ramkb466@gmail.com>
2019-09-30 10:17:17 -0400

    dynamic programing with tabulation

---
+++ dynamicProgTabulation.js
@@ -0,0 +1,134 @@
+// Write a function, stepper(nums), that takes in an array of non negative numbers.
+// Each element of the array represents the maximum number of steps you can take from that position in the array.
+// The function should return a boolean indicating if it is possible to travel from the 
+// first position of the array to the last position.
+//
+// For Example:
+//
+// Given [3, 1, 0, 5, 10]
+//      - We begin at first position, 3. 
+//      - Since the element is 3 we can take up to 3 steps from this position.
+//      - This means we can step to the 1, 0, or 5
+//      - Say we step to 1
+//      - Since the element is 1, now the only option is to take 1 step to land on 0
+//      - etc...
+//
+// Try to solve this in two ways, using tabulation and memoization.
+//
+// Examples:
+//
+// stepper([3, 1, 0, 5, 10]);           // => true, because we can step through elements 3 -> 5 -> 10
+// stepper([3, 4, 1, 0, 10]);           // => true, because we can step through elements 3 -> 4 -> 10
+// stepper([2, 3, 1, 1, 0, 4, 7, 8])    // => false, there is no way to step to the end
+
+
+function stepper(nums) {
+
+    let checker = new Array(nums.length).fill(false);
+    checker[0] = true;
+   for(let i = 0; i < nums.length; i++ ){
+
+        if(checker[i]){ 
+            let range = nums[i]+i+1; // here i+1 is the new position so our range should be value plus that position
+            for(let j = i + 1; ((j < range) && (j < checker.length)); j++){
+                checker[j] = true;
+            }
+        }
+        console.log(checker);
+   }
+   return checker[checker.length -1];
+}
+// console.log(" ans = "+stepper([1,2,0,3,0]))
+
+// function stepper(nums, memo={}) {
+//     let key = String(nums.length);
+//     if(key in memo) return memo[key];
+//    if(nums.length === 0) return true;
+//    let maxRange = nums[0];
+//    for(let step = 1; step <= maxRange; step++){
+//        if(stepper(nums.slice(step), memo)) {
+//            memo[key] = true;
+//            return true;
+//         }
+//    }
+//    memo[key] = false;
+//    return false;
+// }
+//  console.log("ans = "+stepper([1, 2, 0, 3, 0]))
+
+// Write a function, maxNonAdjacentSum(nums), that takes in an array of nonnegative numbers.
+// The function should return the maximum sum of elements in the array we can get if we cannot take
+// adjacent elements into the sum.
+//
+// Try to solve this in two ways, using tabulation and memoization.
+//
+// Examples:
+//
+// maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
+// maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6 
+// function maxNonAdjacentSum(nums) {
+//   let table = new Array(nums.length).fill(0);
+//   table[0] = nums[0];
+//   for(let i = 1; i < table.length; i++){
+//       let skipAdjacentElement = table[i-2] === undefined ? 0 : table[i-2];
+//       // this is the max sum of the elements without adjecent neighbors
+//       let including = skipAdjacentElement + nums[i]; // now include the current element
+//       let excluding = table[i-1]; // max sum upto next element
+//       table[i] = Math.max(including, excluding); // now we need max sum so we compare
+//       // the max between them
+//   }
+//   return table[table.length -1];
+// }
+
+
+function maxNonAdjacentSum(nums, memo = {}) {
+    let key = String(nums.length);
+    if(key in memo) return memo[key];
+   if(nums.length === 0) return 0;
+
+   let first = nums[0];
+   let  including = first + maxNonAdjacentSum(nums.slice(2));
+   let excluding  = maxNonAdjacentSum(nums.slice(1));
+   memo[key] =  Math.max(including, excluding);
+   return memo[key];
+}
+
+// console.log(maxNonAdjacentSum([4, 2, 1, 6]));
+
+// Write a function, minChange(coins, amount), that accepts an array of coin values
+// and a target amount as arguments. The method should the minimum number of coins needed
+// to make the target amount. A coin value can be used multiple times.
+//
+// You've seen this problem before with memoization, but now solve it using the Tabulation strategy!
+//
+// Examples:
+//
+// minChange([1, 2, 5], 11)         // => 3, because 5 + 5 + 1 = 11
+// minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
+// minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
+// minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
+
+
+
+function minChange(coins, amount, memo = {}) {
+    if(amount in memo) return memo[amount]
+    if(amount === 0) return 0;
+    let ans = [];
+    coins.forEach(coin => {
+        if(coin <= amount){
+
+            ans.push(minChange(coins, amount - coin, memo)+1);
+        }
+    })
+    
+    memo[amount] = Math.min(...ans);
+    return memo[amount];
+}
+
+
+// console.log(minChange([1, 2, 5], 11))         // => 3, because 5 + 5 + 1 = 11
+// console.log(minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
+// console.log(minChange([1, 5, 10, 25], 15) )   // => 2, because 10 + 5 = 15
+// console.log(minChange([1, 5, 10, 25], 100) )  // => 4, because 25 + 25 + 25 + 25 = 100
+
+
--- dynamicProgramingMemoization.js
+++ dynamicProgramingMemoization.js
@@ -63,8 +63,8 @@ function minChange(coins, amount, memo= {}) {
    return memo[amount];
 }
 
-console.log(minChange([1, 2, 5], 11))         // => 3, because 5 + 5 + 1 = 11
-console.log(minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
-console.log(minChange([1, 5, 10, 25], 15))    // => 2, because 10 + 5 = 15
-console.log(minChange([1, 5, 10, 25], 100) )
+// console.log(minChange([1, 2, 5], 11))         // => 3, because 5 + 5 + 1 = 11
+// console.log(minChange([1, 4, 5], 8))         // => 2, because 4 + 4 = 8
+// console.log(minChange([1, 5, 10, 25], 15))    // => 2, because 10 + 5 = 15
+// console.log(minChange([1, 5, 10, 25], 100) )
 
