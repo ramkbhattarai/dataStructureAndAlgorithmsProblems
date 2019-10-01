897a75e6ab46dd4be903b4885557cb6a18414cc6
Ram <ramkb466@gmail.com>
2019-10-01 10:06:24 -0400

    inefficient sorting alogrithms but with cool ideas

--- simpleSortingAlgo.js
+++ simpleSortingAlgo.js
@@ -23,6 +23,52 @@
       arr[j] = temp
   }
 
-  console.log(bubbleSort([1]));
+//   console.log(bubbleSort([1]));
 
-  
+  /**
+   * Selection Sort
+   */
+
+   function selectionSort(arr){
+       if (arr.length <= 1) return arr;
+
+       for(let i = 0; i < arr.length; i++){
+           let minIndex = i;
+           for(let j = i+ 1; j < arr.length; j++){
+               if(arr[minIndex] > arr[j]){
+                   minIndex = j;
+               }
+           }
+
+           swap(arr, minIndex, i);
+        //    console.log(arr);
+       }
+       return arr;
+   }
+
+//    console.log(selectionSort([1,5,2,7,4,3,9,0]));
+
+   /**
+    * insertionSort
+    */
+
+    function insertionSort(arr){
+        if (arr.length <= 1) return arr;
+
+        for(let i = 1; i < arr.length; i++){
+            let currEle = arr[i];
+            let j = i-1;
+            while (j >= 0 && arr[j] > currEle){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = currEle;
+            // console.log(arr);
+
+        }
+        return arr;
+    }
+
+// console.log(insertionSort([1, 5, 2, 7, 4, 3, 9, 0]));
+
+
