56fe3a81ed2cd55f26a2b754e78879cdabf5b280
Ram <ramkb466@gmail.com>
2019-10-01 13:24:21 -0400

    mergeSort

--- efficientSortingAlgo.js
+++ efficientSortingAlgo.js
@@ -9,6 +9,33 @@ function quickSort(arr){
 
     return [...quickSort(left), pivot, ...quickSort(right)];
 }
-console.log(quickSort([1,8,9,5,3,2,4,0,6,7]));
+// console.log(quickSort([1,8,9,5,3,2,4,0,6,7]));
 
+/**
+ * MergeSort
+ */
 
+ function mergeSort(arr){
+     if (arr.length <= 1) return arr;
+
+     let midIdx = Math.floor(arr.length /2);
+     let left = arr.slice(0, midIdx);
+     let right = arr.slice(midIdx);
+     return merge(mergeSort(left), mergeSort(right));
+ }
+
+ function merge(left, right){
+     let merged = [];
+     while(left.length || right.length){
+         let ele1 = left.length ? left[0] : Infinity; 
+         let ele2  = right.length ? right[0] : Infinity;
+         if(ele1 < ele2) {merged.push(left.shift());}
+         else {merged.push(right.shift());}
+     }
+     return merged;
+ }
+console.log(mergeSort([1, 8, 9, 5, 3, 2, 4, 0, 6, 7]));
+
+/**
+ * RadixSort
+ */
