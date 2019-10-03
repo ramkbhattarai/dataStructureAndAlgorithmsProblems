7e7c9ef5327abda6482fd3d4a08945411f8a8b6d
Ram <ramkb466@gmail.com>
2019-10-03 07:10:43 -0400

    reversing list from m to n

--- Linked Lists class and its problems/singlyLinkedListProblems.js
+++ Linked Lists class and its problems/singlyLinkedListProblems.js
@@ -39,3 +39,47 @@ function reverse(linkedList){
     curr.next = prev;
     return curr;
 }
+
+/**
+ * Reverse a linked list from position m to n. Do it in one-pass.
+
+Note: 1 ≤ m ≤ n ≤ length of list.
+
+Example:
+
+Input: 1->2->3->4->5->NULL, m = 2, n = 4
+Output: 1->4->3->2->5->NULL
+ */
+
+ function reverseFromMToN(head, m, n){
+     if(!head) return undefined;
+        let curr = head;
+        let prev = null;
+     while(m>1){
+        prev = curr;
+        curr = curr.next;
+        m--;
+        n--;
+     }
+
+     let con = prev;
+     let third = null;
+     let tail = curr;
+
+     while(n>0){
+         third = curr.next;
+         curr.next = prev;
+         prev = curr;
+         curr = third;
+         n--;
+     }
+
+     if(con){
+         con.next = prev;
+     }else{
+         head = prev;
+     }
+
+     tail.next = curr;
+     return head;
+ }
