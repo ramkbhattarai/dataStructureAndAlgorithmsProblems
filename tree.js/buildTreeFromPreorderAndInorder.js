c22df363d271056a72a5f7da7f40f3b481524a19
Ram <ramkb466@gmail.com>
2019-10-06 17:01:16 -0400

    build tree using inorder, preorder, and postorder arrays

---
+++ tree.js/buildTreeFromPreorderAndInorder.js
@@ -0,0 +1,62 @@
+/**
+ * Given preorder and inorder traversal of a tree, construct the binary tree.
+
+Note:
+You may assume that duplicates do not exist in the tree.
+
+For example, given
+
+preorder = [3,9,20,15,7]
+inorder = [9,3,15,20,7]
+Return the following binary tree:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ */
+
+ function buildTree(preorder, inorder){
+     if(!preorder.length && !inorder.length) return null;
+
+     let val = preorder[0];
+     let root = new TreeNode(val);
+
+     let index = inorder.indexOf(val); // we have to take the index from the inorder 
+     // so that we can cover what is in the left of that array to make the left tree.
+
+     let leftInorder = inorder.slice(0, index);
+     let rightInorder = inorder.slice(index + 1);
+
+     let leftPreorder = preorder.filter(ele => leftInorder.includes(ele));
+     let rightPreorder = preorder.filter(ele => rightInorder.includes(ele));
+
+     let leftTree = buildTree(leftPreorder,leftInorder);
+     let rightTree = buildTree(rightPreorder, rightInorder);
+      root.left = leftTree;
+      root.right = rightTree;
+      return root;
+
+ }
+
+var buildTree = function (inorder, postorder) {
+    if (inorder.length === 0 && postorder.length === 0) return null;
+    let val = postorder[postorder.length - 1];
+    let root = new TreeNode(val);
+
+    let index = inorder.indexOf(val);
+
+    let leftInorder = inorder.slice(0, index);
+    let rightInorder = inorder.slice(index + 1);
+    // console.log(leftInorder);
+    // console.log(rightInorder);
+    // console.log("--------------")
+    let leftpost = postorder.filter(ele => leftInorder.includes(ele));
+    let rightpost = postorder.filter(ele => rightInorder.includes(ele));
+    // console.log(leftpost);
+    // console.log(rightpost);
+    root.left = buildTree(leftInorder, leftpost);
+    root.right = buildTree(rightInorder, rightpost);
+    return root;
+};
