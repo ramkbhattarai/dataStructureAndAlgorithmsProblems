0b30b519836cd5156d5ac02e439c93f832cd35e7
Ram <ramkb466@gmail.com>
2019-10-25 18:27:14 -0400

    level travelsal for N-try

--- tree.js/levelTravelsal.js
+++ tree.js/levelTravelsal.js
@@ -35,3 +35,50 @@ var levelOrder = function (root) {
     }
     return ans;
 };
+
+/**
+ * Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example, given a 3-ary tree:
+     1
+   / \ \
+  3  2   4
+    /  \  
+   5   6   
+
+We should return its level order traversal:
+
+[
+     [1],
+     [3,2,4],
+     [5,6]
+]
+ */
+
+/**
+* // Definition for a Node.
+* function Node(val,children) {
+*    this.val = val;
+*    this.children = children;
+* };
+*/
+/**
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+    if (!root) return [];
+    let ans = [];
+    let q = [root];
+    while (q.length) {
+        let level = q.length;
+        let temp = [];
+        for (let i = 0; i < level; i++) {
+            let peek = q[0];
+            if (peek.children.length) q.push(...peek.children);
+            temp.push(q.shift().val)
+        }
+        ans.push(temp);
+    }
+    return ans;
+};
